ce692be57d7635821280835a36835416dbe63fab
fuwei.wei <fuwei.wei@mtime.com>
2016-10-26 14:20:28 +0800

    edit eureka config properties

--- config-server/src/main/java/com/mtime/spring/cloud/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/mtime/spring/cloud/config/ConfigServerApplication.java
@@ -2,18 +2,18 @@ package com.mtime.spring.cloud.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Jason on 2/24/16.
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(ConfigServerApplication.class)
-            .run(args);
-  }
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
+    }
 
 }
